b7cdbaa18b7119ad31c82666125d1e3a55faf6d6
Ricardo Liang <ricardoliang@gmail.com>
2014-03-26 20:47:28 -0300

    Do not show item descriptions; Aligned comments; Rudimentary launcher script included

--- cdmenu.c
+++ cdmenu.c
@@ -95,6 +95,7 @@ void init_form(void)
 void init_menu(void)
 {
 	menu = new_menu(buffer);
+	menu_opts_off(menu, O_SHOWDESC);
 	set_menu_win(menu, newwin(LINES, COLS, 1, 0));
 	post_menu(menu);
 
@@ -147,53 +148,53 @@ void main_loop(void)
 	for (;;) {
 		int key = getch();
 		switch (key) {
-		case 3: // c-c
-		case 27: // esc
+		case 3:    //  c-c
+		case 27:   //  esc
 			return;
-		case 10: // return
+		case 10:   //  return
 			fputs(item_name(current_item(menu)), stderr);
 			return;
-		case 6: // c-f
-		case 9: // tab
-		case 258: // down
+		case 6:    //  c-f
+		case 9:    //  tab
+		case 258:  //  down
 			menu_driver(menu, REQ_NEXT_ITEM);
 			break;
-		case 2: // c-b
-		case 353: // backtab
-		case 259: // up
+		case 2:    //  c-b
+		case 353:  //  backtab
+		case 259:  //  up
 			menu_driver(menu, REQ_PREV_ITEM);
 			break;
-		case 8: // c-h
-		case 260: // left
+		case 8:    //  c-h
+		case 260:  //  left
 			form_driver(form, REQ_PREV_CHAR);
 			break;
-		case 12: // c-l
-		case 261: // right
+		case 12:   //  c-l
+		case 261:  //  right
 			form_driver(form, REQ_NEXT_CHAR);
 			break;
-		case 360: // end
+		case 360:  //  end
 			form_driver(form, REQ_END_LINE);
 			break;
-		case 262: // home
+		case 262:  //  home
 			form_driver(form, REQ_BEG_LINE);
 			break;
-		case 32: // space
+		case 32:   //  space
 			form_driver(form, REQ_INS_CHAR);
 			update_buffer();
 			break;
-		case 330: // delete
+		case 330:  //  delete
 			form_driver(form, REQ_DEL_CHAR);
 			update_buffer();
 			break;
-		case 263: // backspace
+		case 263:  //  backspace
 			form_driver(form, REQ_DEL_PREV);
 			update_buffer();
 			break;
-		case 21: // c-u
+		case 21:   //  c-u
 			form_driver(form, REQ_CLR_FIELD);
 			update_buffer();
 			break;
-		case 23: // c-w
+		case 23:   //  c-w
 			form_driver(form, REQ_DEL_WORD);
 			update_buffer();
 			break;
